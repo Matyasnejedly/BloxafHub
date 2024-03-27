2888d245eaa8ab421bf337022bcfc38258af6e7b
Matyasnejedly <91372244+Matyasnejedly@users.noreply.github.com>
2024-03-27 13:37:33 +0100

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -57,6 +57,24 @@ local UiCornerFrame = Instance.new("UICorner", Frame)
 
 local UiStrokeFrame = Instance.new("UIStroke", Frame)
 
+local DiaChest = Instance.new("TextButton", Frame)
+DiaChest.BackgroundColor3 = Color3.fromRGB(57,136,255)
+DiaChest.Position = UDim2.fromScale(0.145, 0.092)
+DiaChest.Size = UDim2.fromScale(0.709, 0.229)
+DiaChest.Text = "DiaChest"
+
+local GoldChest = Instance.new("TextButton", Frame)
+GoldChest.BackgroundColor3 = Color3.fromRGB(182,195,35)
+GoldChest.Position = UDim2.fromScale(0.145, 0.385)
+GoldChest.Size = UDim2.fromScale(0.709, 0.229)
+GoldChest.Text = "GoldChest"
+
+local BronzChest = Instance.new("TextButton", Frame)
+BronzChest.BackgroundColor3 = Color3.fromRGB(43,24,7)
+BronzChest.Position = UDim2.fromScale(0.145, 0.683)
+BronzChest.Size = UDim2.fromScale(0.709, 0.229)
+BronzChest.Text = "BronzChest"
+
 button.Activated:Connect(function()
 	if Frame.Visible == true then
 		AnimateClose(Frame)
@@ -64,6 +82,41 @@ button.Activated:Connect(function()
 	else
 		Frame.Visible = true
 		AnimateOpen(Frame)
+	end
+end)
+
+BronzChest.Activated:Connect(function()
+	if game.Workspace.Chest1 then
+		BronzChest.Text = "BronzChest Found"
+		TweetPlayer(HumanoidRootPart, game.Workspace.Chest1)
+		BronzChest.Text = "BronzChest"
+	else
+		BronzChest.Text = "BronzChest Not Found"
+		wait(1)
+		BronzChest.Text = "BronzChest"
+	end
+end)
+
+GoldChest.Activated:Connect(function()
+	if game.Workspace.Chest2 then
+		GoldChest.Text = "GoldChest Found"
+		TweetPlayer(HumanoidRootPart, game.Workspace.Chest2)
+		GoldChest.Text = "GoldChest"
+	else
+		GoldChest.Text = "GoldChest Not Found"
+		wait(1)
+		GoldChest.Text = "GoldChest"
+	end
+end)
+
+DiaChest.Activated:Connect(function()
+	if game.Workspace.Chest3 then
+		BronzChest.Text = "DiaChest Found"
 		TweetPlayer(HumanoidRootPart, game.Workspace.Chest3)
+		BronzChest.Text = "DiaChest"
+	else
+		BronzChest.Text = "DiaChest Not Found"
+		wait(1)
+		BronzChest.Text = "DiaChest"
 	end
 end)
